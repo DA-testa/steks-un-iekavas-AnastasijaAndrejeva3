843750b13298a0d18cb8876ab8c382b3d3baec91
AnastasijaAndrejeva3 <125015510+AnastasijaAndrejeva3@users.noreply.github.com>
2023-02-14 19:18:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ def find_mismatch(text):
 
         if next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+                    return i+1
             opening_brackets_stack.pop()
-        if opening_brackets_stack:
-            return opening_brackets_stack[0].position
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    else:
         return "Success"
 
 
